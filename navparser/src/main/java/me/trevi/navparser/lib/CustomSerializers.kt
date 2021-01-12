da597d2e5563414bd6f6a93cf3d3dfa686d71ca8
Marco Trevisan (Treviño) <mail@3v1n0.net>
2021-01-12 01:13:32 +0100

    CustomSerializers: Use easier to parse values for Date and Time

--- navparser/src/main/java/me/trevi/navparser/lib/CustomSerializers.kt
+++ navparser/src/main/java/me/trevi/navparser/lib/CustomSerializers.kt
@@ -4,35 +4,56 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.encoding.encodeStructure
+import kotlinx.serialization.serializer
 import java.io.ByteArrayOutputStream
 import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalTime
 
+@Serializable
+data class LocalTimeSerialDescriptor(val hour : Byte, val minute : Byte, val second : Byte)
+
 object LocalTimeSerializer : KSerializer<LocalTime> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
-        LocalTime::class.java.name,
-        PrimitiveKind.INT
-    )
-    override fun serialize(encoder: Encoder, value: LocalTime) = encoder.encodeInt(value.toSecondOfDay())
-    override fun deserialize(decoder: Decoder): LocalTime = LocalTime.ofSecondOfDay(
-        decoder.decodeInt().toLong()
-    )
+    override val descriptor: SerialDescriptor = LocalTimeSerialDescriptor.serializer().descriptor
+    override fun serialize(encoder: Encoder, value: LocalTime) {
+        encoder.encodeSerializableValue(serializer(),
+            LocalTimeSerialDescriptor(
+                value.hour.toByte(),
+                value.minute.toByte(),
+                value.second.toByte()))
+    }
+    override fun deserialize(decoder: Decoder): LocalTime {
+        decoder.decodeSerializableValue(LocalTimeSerialDescriptor.serializer()).also {
+            return LocalTime.of(it.hour.toInt(), it.minute.toInt(), it.second.toInt())
+        }
+    }
 }
 
+@Serializable
+data class LocalDateSerialDescriptor(val day : Byte, val month : Byte, val year : Short)
+
 object LocalDateSerializer : KSerializer<LocalDate> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
-        LocalDate::class.java.name,
-        PrimitiveKind.LONG
-    )
-    override fun serialize(encoder: Encoder, value: LocalDate) = encoder.encodeLong(value.toEpochDay())
-    override fun deserialize(decoder: Decoder): LocalDate = LocalDate.ofEpochDay(decoder.decodeLong())
+    override val descriptor: SerialDescriptor = LocalDateSerialDescriptor.serializer().descriptor
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeSerializableValue(serializer(),
+            LocalDateSerialDescriptor(
+                value.dayOfMonth.toByte(),
+                value.month.value.toByte(),
+                value.year.toShort()))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        decoder.decodeSerializableValue(LocalDateSerialDescriptor.serializer()).also {
+            return LocalDate.of(it.year.toInt(), it.month.toInt(), it.day.toInt())
+        }
+    }
 }
 
 object DurationSerializer : KSerializer<Duration> {
