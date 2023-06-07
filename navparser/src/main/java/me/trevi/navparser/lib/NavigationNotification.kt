368fd6c0a9b0ccba0725a279a81e1f3a735727e7
Dariusz Baraczuk <dariusz.baranczuk@sigma.se>
2023-06-07 08:58:59 +0200

    Issue #7 Add support for Android 12

--- navparser/src/main/java/me/trevi/navparser/lib/NavigationNotification.kt
+++ navparser/src/main/java/me/trevi/navparser/lib/NavigationNotification.kt
@@ -9,14 +9,16 @@ package me.trevi.navparser.lib
 
 import android.app.Notification
 import android.content.Context
+import android.os.Build
 import android.service.notification.StatusBarNotification
 import android.widget.Button
 
 open class NavigationNotification(cx: Context, sbn: StatusBarNotification) {
     protected val mNotification : Notification = sbn.notification
     protected val mCx = cx
-    protected var mAppSrcCx: Context = mCx.createPackageContext(sbn.packageName,
-        Context.CONTEXT_IGNORE_SECURITY
+    protected var mAppSrcCx: Context = mCx.createPackageContext(
+        if (Build.VERSION.SDK_INT >= 31) SYSTEM_UI_PACKAGE_NAME else sbn.packageName,
+        Context.CONTEXT_IGNORE_SECURITY,
     )
 
     var navigationData : NavigationData = NavigationData()
@@ -31,5 +33,9 @@ open class NavigationNotification(cx: Context, sbn: StatusBarNotification) {
     fun close() {
         stopButton?.callOnClick()
         navigationData.actionIcon.bitmap?.recycle()
+    }
+
+    companion object {
+        private val SYSTEM_UI_PACKAGE_NAME = "com.android.systemui"
     }
 }
